d041157811e3435eb4f53712dedd9594de42f4f4
nhat khoa <nhatkhoa.tin@gmail.com>
2021-04-15 14:40:15 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,64 @@
+
+import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import WeatherInfo from './components/WeatherInfo';
+import Navbar from "./components/NavigationBar";
+import SideBar from './components/SideBar';
+require('dotenv').config();
+
+
+
+function App() {
+  const [weather, setWeather] = useState(null);
+  const [apiError, setAPIError] = useState("");
+  const API_KEY = process.env.REACT_APP_API_KEY;
+
+
+  const getUserLocation = () => {
+  navigator.geolocation.getCurrentPosition((position) => {
+    const { latitude, longitude } = position.coords;
+    getWeatherByCurrentLocation(latitude, longitude);
+  });
+};
+
+  useEffect(() => {
+    getUserLocation();
+  },[]);
+
+  const getWeatherByCurrentLocation = async (lat, lon) => {
+    try {
+      let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+      const res = await fetch(url);
+      const data = await res.json();
+      console.log("weather data", data);
+      setWeather(data);
+    } catch (err) {
+      setAPIError(err.message);
+    }
+  };
+
+  if(!weather){
+    return <div className="loading">  
+      <div className="obj"></div>
+      <div className="obj"></div>
+      <div className="obj"></div>
+      <div className="obj"></div>
+      <div className="obj"></div>
+      <div className="obj"></div>
+      <div className="obj"></div>
+      <div className="obj"></div>
+    </div>
+  }
+
+
+  return (
+    <div className="app">
+      <SideBar />
+      <Navbar />
+      <WeatherInfo weather={weather} />
+    </div>
+  );
+
+}
+export default App;
---
+++ src/components/WeatherInfo.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {Card, Container } from 'react-bootstrap';
+import './WeatherInfo.css'
+
+const WeatherInfo = ({ weather }) => {
+    const temperatureC =
+        weather && weather.main ? (weather.main.temp - 273.15).toFixed(2) : "";
+    const temperatureF =
+        weather && weather.main
+        ? (((weather.main.temp - 273.15) * 9) / 5 + 32).toFixed(2)
+        : "";
+
+
+
+    return (
+        <Container className="mx-auto my-4 py-4" >
+            <Card className="bg-dark text-white main-content">
+                <Card.ImgOverlay className="d-flex flex-column justify-content-center text-center">
+                    <div className="d-flex justify-content-center"> 
+                        <Card.Title className="text-content">{weather?.name}</Card.Title>
+                    </div>
+                    <Card.Text className="text-success h1">
+                        {`${temperatureC} °C / ${temperatureF} °F`}
+                    </Card.Text>
+                    <Card.Text className="text-info text-uppercase h2">
+                        {weather && weather.weather[0]?.description}
+                    </Card.Text>
+                </Card.ImgOverlay>
+            </Card>
+        </Container>
+);
+};
+
+export default WeatherInfo;
